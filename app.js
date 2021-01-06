8cd1657a19c51ce21c9f130a82f2b0d3ca7de9d7
Soria <soriasayer@gmail.com>
2021-01-06 14:30:32 +0100

    Redirecting requests and parsing request bodies.

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  const url = req.url
+  const method = req.method
+  if(url === '/'){
+    res.write('<html>')
+    res.write('<head><title>Enter Message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  if(url === '/message' && method === 'POST') {
+    const body = []
+    // on listens to an event
+    req.on('data', chunk => {
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString()
+      const message = parseBody.split('=')[1]
+      fs.writeFileSync('message.txt', message)
+    })
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
+  res.setHeader('Content-Type', 'text/html')
 })
 
 server.listen(3000)
