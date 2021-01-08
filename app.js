509374ebb998c979cd818d68aab0ecacfaf63c13
Soria <soriasayer@gmail.com>
2021-01-08 15:45:57 +0100

    Adding styles to the pages.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const shopRoute = require('./routes/shop')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin',adminRoute)
 app.use(shopRoute)
