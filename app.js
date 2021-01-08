501efa18b0f42603fb12c212dde17113fa9420dd
Soria <soriasayer@gmail.com>
2021-01-08 10:30:02 +0100

    Creating markups for shop and add product pages.

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoute)
+app.use('/admin',adminRoute)
 app.use(shopRoute)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/add-product', (req, res, next) => {
-  res.send('<form action="product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  res.send('<form action="/add=product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
   res.redirect('/')
 })
