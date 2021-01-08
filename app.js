26d6d8e679f03c58509769a7088b3b692b955752
Soria <soriasayer@gmail.com>
2021-01-08 13:50:43 +0100

    Creating routes with shop and add-product pages.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
@@ -12,7 +13,7 @@ app.use('/admin',adminRoute)
 app.use(shopRoute)
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
-router.use('/add-product', (req, res, next) => {
-  res.send('<form action="/add=product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
