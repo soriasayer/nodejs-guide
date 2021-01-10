66dec57b9f2b4522bd8f73bf9dd0f38d734c93fb
Soria <soriasayer@gmail.com>
2021-01-10 20:59:45 +0100

    Creating file storage on modules.

--- model/product.js
+++ model/product.js
@@ -1,14 +1,37 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+)
+
+const readProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if(err) {
+        cb([])
+    } else {
+      cb(JSON.parse(fileContent))
+    }
+  })
+}
+
 module.exports = class Product {
   constructor(t){
     this.title = t
   }
 
   save() {
-    products.push(this)
+   readProductsFromFile(products => {
+     products.push(this)
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err)
+      })
+   })
   }
 
-  static fetchAll() {
-    return products
+  static fetchAll(cb) {
+    readProductsFromFile(cb)
   }
 }
