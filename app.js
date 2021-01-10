892998e0fc2d6ea98c14d903f40e0f586f19933f
Soria <soriasayer@gmail.com>
2021-01-10 14:03:58 +0100

    working on dynamic button color and title.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+cosnt expressHbs = require('express-handlebars')
 const path = require('path')
 
 const adminRoute = require('./routes/admin')
@@ -7,6 +8,7 @@ const shopRoute = require('./routes/shop')
 
 const app = express()
 
+app.engine()
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
@@ -17,7 +19,8 @@ app.use('/admin',adminRoute.routes)
 app.use(shopRoute)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+  res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  // .sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000)
--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,7 @@ const router = express.Router()
 const products = []
 
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product')
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+  res.render('add-product', {pageTitle: 'Add Product', path: 'admin/add-product'}   )
 })
 
 router.post('/add-product', (req, res, next) => {
