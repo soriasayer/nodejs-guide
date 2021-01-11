b928ebe057d1ed42426c4e0f7019d1e8249222ca
Soria <soriasayer@gmail.com>
2021-01-11 15:23:49 +0100

    Creating new views and routes to the application.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../model/product')
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   })
@@ -15,10 +15,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-      res.render('shop', {
+      res.render('admin/products', {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     })
   })
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const path = require('path')
-const productsContorller = require('../controllers/products')
+const adminController = require('../controllers/admin')
 
 const router = express.Router()
 
-router.get('/add-product', productsContorller.getAddProduct)
+router.get('/add-product', adminController.getAddProduct)
+router.get('/edit-product', adminController.getAddProduct)
+router.get('/products', adminController.getAddProduct)
 
-router.post('/add-product', productsContorller.postAddProduct)
+router.post('/add-product', adminController.postAddProduct)
 
 module.exports = router
