efe8a74fee6aca9e5c7ad2569e4674c4616d49af
Soria <soriasayer@gmail.com>
2021-01-11 16:25:50 +0100

    Adding more values to the product.

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,8 @@ const adminController = require('../controllers/admin')
 const router = express.Router()
 
 router.get('/add-product', adminController.getAddProduct)
-router.get('/edit-product', adminController.getAddProduct)
-router.get('/products', adminController.getAddProduct)
+router.get('/edit-product')
+router.get('/products', adminController.getProducts)
 
 router.post('/add-product', adminController.postAddProduct)
 
